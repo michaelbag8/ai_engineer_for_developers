2197ee88f4f9af73ba16001a8a505a0c06bb04bd
Michael Bag <michaelbag8@gmail.com>
2026-08-07 21:16:38 +0000

    introduction to openai

---
+++ intro.py
@@ -0,0 +1,14 @@
+#Introduction to OpenAI
+from openai import OpenAI
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+response = client.chat.completions.create(
+  # Specify the model
+    model="gpt-4o-mini",
+      messages=[
+          # Assign the correct role
+              {"role": "user", 
+                   "content": "Announce my new AI Engineer role on LinkedIn."}]
+                   )
+
+print(response.choices[0].message.content)
