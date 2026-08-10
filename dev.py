dfd33edacf424e0dfae0b0de24a20a9a842329c4
Michael Bag <michaelbag8@gmail.com>
2026-08-10 19:44:26 +0100

    Add range and list unpacking examples in dev.py
    Added examples of range and list unpacking.

--- dev.py
+++ dev.py
@@ -9,3 +9,15 @@ names_uppercase = [*names_map]
 
 # Print the list created above
 print(names_uppercase)
+
+# Create a range object that goes from 0 to 5
+nums = range(6)
+print(type(nums))
+
+# Convert nums to a list
+nums_list = list(nums)
+print(nums_list)
+
+# Create a new list of odd numbers from 1 to 11 by unpacking a range object
+nums_list2 = [*range(1,11,2)]
+print(nums_list2)
