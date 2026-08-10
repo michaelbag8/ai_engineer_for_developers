5e66fd909281243723358b462dad93f3a47adca7
Michael Bag <michaelbag8@gmail.com>
2026-08-10 19:45:27 +0100

    Rewrite for loop with enumerate and list comprehension
    Refactor for loop to use enumerate and list comprehension.

--- dev.py
+++ dev.py
@@ -21,3 +21,18 @@ print(nums_list)
 # Create a new list of odd numbers from 1 to 11 by unpacking a range object
 nums_list2 = [*range(1,11,2)]
 print(nums_list2)
+
+# Rewrite the for loop to use enumerate
+indexed_names = []
+for i,name in enumerate(names):
+    index_name = (i,name)
+    indexed_names.append(index_name) 
+print(indexed_names)
+
+# Rewrite the above for loop using list comprehension
+indexed_names_comp = [(i,name) for i,name in enumerate(names)]
+print(indexed_names_comp)
+
+# Unpack an enumerate object with a starting index of one
+indexed_names_unpack = [*enumerate(names, 1)]
+print(indexed_names_unpack)
